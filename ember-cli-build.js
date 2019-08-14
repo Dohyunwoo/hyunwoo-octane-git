23176302b7bca6760b36c472c48419c260362935
Dohyunwoo <ehgusfn12@gmail.com>
2019-08-14 20:21:31 +0900

    uikit test

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,10 +3,11 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
-    // Add options here
-  });
-
+  let app = new EmberApp(defaults, {});
+  
+  app.import('node_modules/uikit/dist/css/uikit.css');
+  app.import('node_modules/uikit/dist/js/uikit.js');
+ 
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
